818acfd64cd4e7e055924e5d76dfff858154385c
pierrbt <pierre08bt@gmx.co.uk>
2022-08-08 00:09:24 +0200

    added logout.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php session_start(); ?>
+<?php session_start();
+        require_once 'tools.php'?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,9 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>MyProject : Accueil</title>
-    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <h1>MyProject - Accueil</h1>
     <!-- PAGE D'ACCUEIL, JE TE LAISSE FAIRE -->
+    <?php if(isset($_SESSION['user'])) : ?>
+        <p>Bienvenue <?=getPseudo($_SESSION['id'])?> !</p>
+        <a href="account.php">Mon compte</a>
+        <a href="logout.php">Se déconnecter</a>
+    <?php else : ?>
+        <p>Bienvenue sur MyProject !</p>
+        <a href="login.php">Se connecter</a>
+        <a href="register.php">S'inscrire</a>
+    <?php endif; ?>
 </body>
