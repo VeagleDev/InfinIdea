52e30b15354ceb597c20e10c89823b379fc61b29
pierrbt <pierre08bt@gmx.co.uk>
2022-09-03 00:33:31 +0200

    création write.php

--- write.php
+++ write.php
@@ -26,3 +26,39 @@ ET TU CHANGERAS LA VALEUR DE article AVEC LA FONCTION QUI SERA SUREMENT EN JavaS
 -->
 
 <?php
+
+if(session_status() == PHP_SESSION_NONE)
+{
+    session_start(); // On démarre la session AVANT toute chose
+}
+require_once 'autoconnect.php';
+require_once 'tools.php';
+
+// si l'utilisateur est connecté
+if(isset($_POST['title']) && isset($_POST['content']))
+{
+    echo "Publication de l'article...";
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $author = $_SESSION['id'];
+    echo "Article de " . getPseudo($author) . " : " . $title . " : " . $content;
+}
+else
+{
+    // si l'utilisateur a envoyé un article
+    if (!isset($_SESSION['id'])) {
+        echo "Il faut être connecté pour publier un article !";
+        die();
+    }
+    ?>
+    <h1>MyProject - Article</h1>
+    <p>Bienvenue sur MyProject, <?=getPseudo($_SESSION['id'])?>  sur la page d'écriture !</p>
+    <!-- formulaire avec champs pour le titre, et le contenu de l'article avec un bouton submit-->
+    <form action="write.php" method="post">
+        <input type="text" name="title" placeholder="Titre de l'article">
+        <textarea name="content" id="content" cols="30" rows="10" placeholder="Contenu de l'article"></textarea>
+        <input type="submit" value="Publier">
+    </form>
+    <?php
+
+}
