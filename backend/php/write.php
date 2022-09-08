2d1030fbe7a40b0892dc7556238375604afa7fb2
Pierre <pierre08bt@gmx.co.uk>
2022-09-08 22:33:31 +0200

    banner

--- backend/php/write.php
+++ backend/php/write.php
@@ -67,12 +67,13 @@ if(isset($_POST['title']) && isset($_POST['content']))
     $result = mysqli_query($db, $sql);
     if($result)
     {
+
         echo "Article publié";
         // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
         if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0)
         {
             // Testons si le fichier n'est pas trop gros
-            if ($_FILES['screenshot']['size'] <= 1000000)
+            if ($_FILES['screenshot']['size'] <= 8000000)
             {
                 // Testons si l'extension est autorisée
                 $fileInfo = pathinfo($_FILES['screenshot']['name']);
