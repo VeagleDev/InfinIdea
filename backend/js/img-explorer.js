3c48e3580156e4227cc73c3951b02b02c50fec65
Nicolasse <thecrinten@gmail.com>
2022-09-11 01:54:28 +0200

    aall

--- backend/js/img-explorer.js
+++ backend/js/img-explorer.js
@@ -1,8 +1,13 @@
 const container = document.getElementsByClassName("preview-container")[0]
+document.getElementsByClassName("preview")[0].style.border = "#000 2px solid"
 
 container.addEventListener('click', (event) => {
     const displayedImg = document.getElementById("displayed-img")
 
-    if(event.target.classList.contains("preview"))
-    displayedImg.src = event.target.src
+    if(event.target.classList.contains("preview") && event.target.src != displayedImg.src) {
+        displayedImg.src = event.target.src
+        event.target.classList.add("glow")
+    }
 })
+
+document.g
