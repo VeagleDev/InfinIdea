6f2a0fc3bc1b37b6ff613b4484a215a761da55cb
Pierre <pierre08bt@gmx.co.uk>
2022-09-11 01:01:20 +0200

    fix new like method

--- tools/like.js
+++ tools/like.js
@@ -1,8 +1,8 @@
 const LikeMachine = new XMLHttpRequest();
 const LikeRefresher = new XMLHttpRequest();
-
+aid = 0;
 function performLike( id ) {
-
+    aid = id;
     const url = "https://myproject.mysteriousdev.fr/article.php?id=" + id + "&action=like";
     console.log("[INFO] Sending like request to " + url);
     LikeMachine.open("GET", url);
@@ -21,8 +21,8 @@ LikeMachine.onreadystatechange = function() {
     }
 }
 
-function refreshLikeCounter() {
-    const url = "https://myproject.mysteriousdev.fr/tools/infos.php?like=" + id;
+function refreshLikeCounter( id ) {
+    const url = "https://myproject.mysteriousdev.fr/tools/infos.php?like=" + aid;
     console.log("[INFO] Refreshing like counter");
     LikeRefresher.open("GET", url);
     LikeRefresher.send();
