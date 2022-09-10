078334ad84bbba0099f19cc138ac698dae6fc412
Pierre <pierre08bt@gmx.co.uk>
2022-09-11 00:57:57 +0200

    fix new like method

--- tools/infos.php
+++ tools/infos.php
@@ -2,29 +2,28 @@
 require_once 'tools/tools.php';
 
 
-if(isset($_GET['info']))
+if(isset($_GET['like']))
 {
-    $param = htmlspecialchars($_GET['info']);
-    if($param == 'likes')
+    $param = htmlspecialchars($_GET['like']);
+
+    $db = getDB();
+    $sql = "SELECT likes FROM articles WHERE id = " . $_GET['like'];
+    $result = mysqli_query($db, $sql);
+    if($result)
     {
-        $db = getDB();
-        $sql = "SELECT likes FROM articles WHERE id = " . $_GET['article'];
-        $result = mysqli_query($db, $sql);
-        if($result)
+        $row = mysqli_fetch_assoc($result);
+        if($row)
         {
-            $row = mysqli_fetch_assoc($result);
-            if($row)
-            {
-                echo $row['likes'];
-            }
-            else
-            {
-                echo "0";
-            }
+            echo $row['likes'];
         }
         else
         {
             echo "0";
         }
     }
+    else
+    {
+        echo "0";
+    }
+
 }
