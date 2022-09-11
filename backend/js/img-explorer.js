f917b0dd4ba84709fb36eedf8a6c8959cf1db8fc
Nicolasse <thecrinten@gmail.com>
2022-09-11 21:00:24 +0200

    all

--- backend/js/img-explorer.js
+++ backend/js/img-explorer.js
@@ -1,13 +1,16 @@
 const container = document.getElementsByClassName("preview-container")[0]
-document.getElementsByClassName("preview")[0].style.border = "#000 2px solid"
 
 container.addEventListener('click', (event) => {
     const displayedImg = document.getElementById("displayed-img")
 
-    if(event.target.classList.contains("preview") && event.target.src != displayedImg.src) {
+    event.target.classList.remove("glow")
+
+    for(var i = 0; i < container.children.length; i++) {
+        document.getElementsByClassName("preview")[i].classList.remove("glow")
+    }
+
+    if(event.target.classList.contains("preview")) {
         displayedImg.src = event.target.src
         event.target.classList.add("glow")
     }
 })
-
-document.g
