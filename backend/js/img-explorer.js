e73b3372328464109aca08e36052019ed8f5165b
pierrbt <contact@mysteriousdev.fr>
2022-09-12 12:37:17 +0200

    js

--- backend/js/img-explorer.js
+++ backend/js/img-explorer.js
@@ -1,15 +1,17 @@
 const container = document.getElementsByClassName("preview-container")[0]
 
+document.getElementsByClassName("preview")[0].classList.add("glow")
+
 container.addEventListener('click', (event) => {
     const displayedImg = document.getElementById("displayed-img")
 
     event.target.classList.remove("glow")
 
-    for(var i = 0; i < container.children.length; i++) {
+    for (var i = 0; i < container.children.length; i++) {
         document.getElementsByClassName("preview")[i].classList.remove("glow")
     }
 
-    if(event.target.classList.contains("preview")) {
+    if (event.target.classList.contains("preview")) {
         displayedImg.src = event.target.src
         event.target.classList.add("glow")
     }
