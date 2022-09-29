04ec9ac31a97bd7ab4d604da715f0143c60fee97
Pierre <pierre08bt@gmx.co.uk>
2022-09-29 18:31:04 +0200

    new explore.php

--- explore.php
+++ explore.php
@@ -1,15 +1,16 @@
 <?php
-set_include_path('/var/www/blog/');
+set_include_path('/var/www/blog');
 if(session_status() == PHP_SESSION_NONE)
 {
     session_start(); // On démarre la session AVANT toute chose
 }
-
 require_once 'account/autoconnect.php';
-
 require_once 'tools/tools.php';
 
+$db = getDB();
+
 ?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -25,87 +26,135 @@ require_once 'tools/tools.php';
     <section class="top-page">
         <header>
             <nav class="top-nav">
-                <img src="images/logo.png" alt="MysteriousDevelopers creation" class="logo-top">
+                <img src="images/logo_veagle_white.png" alt="MysteriousDevelopers creation" class="logo-top">
                 <ul class="main-list">
-                    <li class="first-child"><a href="index.php"><p>Accueil &nbsp;<i class="fa-solid fa-angle-down"></i></p></a></li>
-                    <li class="first-child"><a href="article.php?type=recommanded"><p>Recommendations &nbsp;<i class="fa-solid fa-angle-down"></i></p></a></li>
-                    <li class="first-child"><a href="article.php?type=recents"><p>Lus récemmment &nbsp;<i class="fa-solid fa-angle-down"></i></p></a></li>
-                    <li class="first-child"><a href="about.html"><p>A propos &nbsp;<i class="fa-solid fa-angle-down"></i></p></a></li>
+                    <li class="first-child"><a href="#"><p><i class="fa-solid fa-house nav-icon"></i> Accueil</p></a></li>
+                    <li class="first-child"><a href="explore.php?type=recommandations"><p><i class="fa-solid fa-shuffle nav-icon"></i> Recommendations</p></a></li>
+                    <li class="first-child"><a href="explore.php?type=recents"><p><i class="fa-regular fa-heart nav-icon"></i> Suivis</p></a></li>
+                    <li class="first-child"><a href=""><p><i class="fa-solid fa-question nav-icon"></i> A propos</p></a></li>
                 </ul>
             </nav>
             <nav class="user-connection-interaction-nav">
                 <ul class="user-connection-interaction-list">
-                    
-                            
-                                <?php if(isset($_SESSION['id']))
-                                {
-                                    ?>
-                                    <li class="user-menu">
-                        <a href="">
-                            <?php
-                                    echo('<p>Bonjour, &nbsp;<p class="unconnected">' . getPseudo($_SESSION['id']) . '&nbsp;<i class="fa-solid fa-angle-down"></i></p></p>');
-                                ?>
+                    <li class="user-menu">
+                        <a href="account/login.php">
+                            <p>Bonjour, &nbsp;
 
-                                    <ul class="user-connection-scrolling-menu">
-                                        <li><a href="account/account.php"><p>Mon compte</p></a></li>
-                                        <li><a href="about.html"><p>A propos</p></a></li>
-                                        <li><a href="explore.php?type=own"><p>Mes projets</p></a></li>
-                                        <li><a href=""><p>Conditions d'utilisations</p></a></li>
-                                        <li><a href="account/account.php"><p>Paramètres</p></a></li>
-                                        <li><a href="account/logout.php"><p>Déconnexion</p></a></li>
-                                    </ul>
-                                    </a>                       
-                    </li>
                                 <?php
-                                }                                  
+                                if(isset($_SESSION['id'])) // Si l'utilisateur est connecté, on affiche son pseudo
+                                {
+                                    echo('<p class="unconnected">' . getPseudo($_SESSION['id']) . '<i class="fa-solid fa-angle-down arrow"></i></p>');
+                                }
+                                else // Sinon on affiche qu'il faut de connecter
+                                {
+                                    echo('<p class="unconnected">connectez-vous<i class="fa-solid fa-angle-down arrow"></i></p>');
+                                }
                                 ?>
-                            
-                            
-                       
-                    
-                    
 
+                            </p>
+                            <ul class="user-connection-scrolling-menu">
+                                <li><a href="account/account.php"><p>Mon compte</p></a></li>
+                                <li><a href=""><p>A propos</p></a></li>
+                                <li><a href=""><p>Mes projets</p></a></li>
+                                <li><a href="account/account.php"><p>Paramètres</p></a></li>
+                                <li><a href="account/logout.php"><p>Déconnexion</p></a></li>
+                            </ul>
+                        </a>
+                    </li>
                     <?php
-                        if(!isset($_SESSION['id']))
-                        {
-                            echo '<a href="account/login.php?redirect=explore.php" class="sign-in unconnected"><li class="sign-in-sub-element"><p>Se connecter</p></li></a>';
-                            echo '<a href="account/register.php" class="sign-in unconnected"><li class="sign-in-sub-element"><p>S\'inscrire</p></li></a>';
-                        }
+                    if(!isset($_SESSION['id'])) // Si il n'est pas connecté, on lui propose de se connecter
+                    {
+                        echo('<a href="account/register.php" class="sign-in unconnected"><li class="sign-in-sub-element"><p>S\'inscrire</p></li></a>');
+                    }
                     ?>
-                    
                 </ul>
             </nav>
         </header>
     </section>
 
-    <section class="contents-page">
-        <div class="card-container">
+    <section class="page-content">
+        <div class="explorer-container">
 
-            <?php
-                $db = getDB();
-                $sql = "SELECT * FROM articles ORDER BY created DESC LIMIT 16;";
-                $result = mysqli_query($db, $sql);
-                while($row = mysqli_fetch_assoc($result))
-                {
-                    echo('
 
-                        <a href="article.php?id=' . $row['id'] . '" class="card">
-                        <img src="images/uploads/' . $row['id'] . '.jpg" class="presentation-photo">
-                        <div class="text-container">
-                            <h3>' . $row['name'] . '</h3>
-                            <div class="statistics-container">
-                                <p class="views-number">' . $row['views'] . ' vues</p>
-                                <p class="date-upload">' . correctTimestamp($row['created']) . '</p>
+            <div class="gallery js-flickity"
+            data-flickity-options='{ "wrapAround": true }'>
+
+                <?php
+                    $sql = "SELECT * FROM articles ORDER BY views DESC LIMIT 20";
+                    $result = mysqli_query($db, $sql);
+                    while($row = mysqli_fetch_assoc($result))
+                    { ?>
+                        <a href="article.php?id=<?php echo($row['id']); ?>" class="gallery-cell">
+
+                            <div class="img-container">
+                                <div><img src="images/uploads/<?php echo($row['id']); ?>.jpg" alt=""></div>
                             </div>
-                            <p class="description">' . $row['description'] . '</p>
-                        </div>            
-                    </a>
 
-                    ');
-                }
+                            <div class="text">
+                                <h1><?php echo($row['name']); ?></h1>
+                                <p class="description"><?php echo($row['description']); ?></p>
+                                <p class="description"><?php echo($row['content']); ?></p>
+                            </div>
+                        </a>
+                    <?php }
 
-            ?>
+                ?>
+
+
+            </div>
         </div>
+
+
     </section>
+
+    <section class="bottom-page">
+        <footer>
+            <div class="site-nav">
+                <nav>
+                    <ul>
+                        <li><p class="nav-title">Soutien</p></li>
+                        <li><a href=""><p>Nous contacter</p></a></li>
+                        <li><a href=""><p>A propos</p></a></li> 
+                    </ul>
+                </nav>
+                <nav>
+                    <ul>
+                        <li><p class="nav-title">Contactez-nous</p></li>
+                        <li><a href="https://discord.gg/Vahr76XmpU" target="_blank"><p>Discord</p></a></li>
+                        <li><a href=""><p>Mail</p></a></li>
+                        <li><a href="https://www.instagram.com/nicolas_fsn_/" target="_blank"><p>Instagram</p></a></li>
+                    </ul>
+                </nav>
+                <nav>
+                    <ul>
+                        <li><p class="nav-title">Rejoignez-nous</p></li>
+                        <li><a href="https://veagle.fr" target="_blank"><p>veagle.fr</p></a></li>
+                        <li><a href="https://discord.gg/Vahr76XmpU" target="_blank"><p>Discord</p></a></li>
+                        <li><a href="https://www.instagram.com/nicolas_fsn_/"  target="_blank"><p>Instagram</p></a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="copyright-infos-nav">
+                <nav>
+                    <ul>
+                        <li><p>© 2022 InfinIdea, by VEagle</p></li>
+                        <li><a href="">Politique de confidentialité</a></li>
+                    </ul>
+                </nav>
+                <nav class="social-media">
+                    <ul>
+                        <li><a href="https://discord.gg/Vahr76XmpU" target="_blank"><i class="fa-brands fa-discord"></i></a></li>
+                        <li><a href="https://www.instagram.com/nicolas_fsn_/" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
+                        <li><a href="https://github.com/Mysterious-Developers" target="_blank"><i class="fa-brands fa-github"></i></a></li>
+                        <li><a href="https://veagle.fr" target="_blank"><i class="fa-solid fa-window-restore"></i></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+    </section>
+
+    <script src="backend/js/main.js"></script>
+    <script src="backend/js/flickity.pkgd.min.js"></script>
+    <script src="backend/js/grid-check-ratio.js"></script>
 </body>
 </html>
