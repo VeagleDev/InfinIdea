9db10573a098160f44b5183826bf705edd80089d
Nicolasse <thecrinten@gmail.com>
2022-09-29 19:37:30 +0200

    all

--- backend/js/grid-check-ratio.js
+++ backend/js/grid-check-ratio.js
@@ -1,4 +1,6 @@
 document.querySelectorAll(".img-article").forEach((el) => {
+    console.log(el.offsetWidth + "width")
+    console.log(el.offsetHeight + "height")
     if(el.offsetWidth / el.offsetHeight <= 1) {
         el.style.height = "100%"
         el.style.width = "auto"
@@ -6,32 +8,5 @@ document.querySelectorAll(".img-article").forEach((el) => {
     } else {
         el.style.height = "auto"
         el.style.width = "100%"
-    }
-})
-
-if(width / height <= 1) {
-    document.getElementById("displayed-img").style.height = "100%"
-    document.getElementById("displayed-img").style.width = "auto"
-    console.log("vertical")
-} else {
-    document.getElementById("displayed-img").style.height = "auto"
-    document.getElementById("displayed-img").style.width = "100%"
-    console.log("horizontal" + width / height)
-}
-
-document.getElementsByClassName("preview-container")[0].addEventListener("click", () => {
-    var newWidth = document.getElementById("displayed-img").offsetWidth
-    var newHeight = document.getElementById("displayed-img").offsetHeight
-
-    if(newWidth / newHeight <= 1) {
-        document.getElementById("displayed-img").style.height = "100%"
-        document.getElementById("displayed-img").style.width = "auto"
-        
-        document.getElementsByClassName("displayed-img")[0].style.height = "450px"
-    } else {
-        document.getElementById("displayed-img").style.height = "auto"
-        document.getElementById("displayed-img").style.width = "100%"
-
-        document.getElementsByClassName("displayed-img")[0].style.height = "450px"
     }
 })
