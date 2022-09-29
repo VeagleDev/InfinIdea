5546b6c8f3232605036e23ff9c9e9085aacebcba
Nicolasse <thecrinten@gmail.com>
2022-09-29 20:07:41 +0200

    all

--- backend/js/grid-check-ratio.js
+++ backend/js/grid-check-ratio.js
@@ -1,12 +1,12 @@
 document.querySelectorAll(".img-article").forEach((el) => {
-    console.log(el.offsetWidth + "width")
-    console.log(el.offsetHeight + "height")
     if(el.offsetWidth / el.offsetHeight <= 1) {
         el.style.height = "100%"
         el.style.width = "auto"
+        console.log("vertical")
 
     } else {
         el.style.height = "auto"
         el.style.width = "100%"
+        console.log("horizontal")
     }
 })
