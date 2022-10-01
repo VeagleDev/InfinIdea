13fa651af69ab15bfd53772d558e0002b86f4b2b
Pierre <pierre08bt@gmx.co.uk>
2022-10-01 23:58:01 +0200

    debug & avancée du systeme d'écriture

--- index.php
+++ index.php
@@ -20,7 +20,54 @@
     <link rel="stylesheet" href="css/index.css">
     <script src="backend/js/fontawesome.js"></script>
 </head>
+<section class="top-page">
+    <header>
+        <nav class="top-nav">
+            <img src="images/logo_veagle_white.png" alt="MysteriousDevelopers creation" class="logo-top">
+            <ul class="main-list">
+                <li class="first-child"><a href="#"><p><i class="fa-solid fa-house nav-icon"></i> Accueil</p></a></li>
+                <li class="first-child"><a href="explore.php?type=recommandations"><p><i class="fa-solid fa-shuffle nav-icon"></i> Recommendations</p></a></li>
+                <li class="first-child"><a href="explore.php?type=recents"><p><i class="fa-regular fa-heart nav-icon"></i> Suivis</p></a></li>
+                <li class="first-child"><a href=""><p><i class="fa-solid fa-question nav-icon"></i> A propos</p></a></li>
+            </ul>
+        </nav>
+        <nav class="user-connection-interaction-nav">
+            <ul class="user-connection-interaction-list">
+                <li class="user-menu">
+                    <a href="account/login.php">
+                        <p>Bonjour, &nbsp;
 
+                            <?php
+                            if(isset($_SESSION['id'])) // Si l'utilisateur est connecté, on affiche son pseudo
+                            {
+                                echo('<p class="unconnected">' . getPseudo($_SESSION['id']) . '<i class="fa-solid fa-angle-down arrow"></i></p>');
+                            }
+                            else // Sinon on affiche qu'il faut de connecter
+                            {
+                                echo('<p class="unconnected">connectez-vous<i class="fa-solid fa-angle-down arrow"></i></p>');
+                            }
+                            ?>
+
+                        </p>
+                        <ul class="user-connection-scrolling-menu">
+                            <li><a href="account/account.php"><p>Mon compte</p></a></li>
+                            <li><a href=""><p>A propos</p></a></li>
+                            <li><a href=""><p>Mes projets</p></a></li>
+                            <li><a href="account/account.php"><p>Paramètres</p></a></li>
+                            <li><a href="account/logout.php"><p>Déconnexion</p></a></li>
+                        </ul>
+                    </a>
+                </li>
+                <?php
+                if(!isset($_SESSION['id'])) // Si il n'est pas connecté, on lui propose de se connecter
+                {
+                    echo('<a href="account/register.php" class="sign-in unconnected"><li class="sign-in-sub-element"><p>S\'inscrire</p></li></a>');
+                }
+                ?>
+            </ul>
+        </nav>
+    </header>
+</section>
 
     <section class="contents-page">
         <div class="welcom-display">
