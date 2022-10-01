d752f856a339b999c37cc541064a8d6c6aee4319
Pierre <pierre08bt@gmx.co.uk>
2022-10-01 14:13:09 +0200

    uploading image

--- TESTING/upload-image.php
+++ TESTING/upload-image.php
@@ -6,7 +6,8 @@ if(isset($_POST['base64']))
     $base_to_php = explode(',', $img);
     $data = base64_decode($base_to_php[1]);
     file_put_contents("test-image.jpg",$data);
-    echo("Image uploaded : test-image.jpg");
+    $path = 'https://infinidea.veagle.fr/TESTING/test-image.jpg';
+    echo $path;
 }
 else
 {
