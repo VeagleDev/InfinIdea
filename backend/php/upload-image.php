9649eb36e24f15cb8ab4826f1acbe4de67cad6a7
Pierre <pierre08bt@gmx.co.uk>
2022-10-06 21:00:10 +0200

    debug & avancée du systeme d'écriture

--- backend/php/upload-image.php
+++ backend/php/upload-image.php
@@ -26,15 +26,11 @@ if(isset($_POST['image']) && isset($_POST['article']))
             // create an unique id for the image
             $id = uniqid();
             $sql = "INSERT INTO images(uid, aid) VALUES('" . $id . "', " . $article . ")";
-
-            echo $article . ' : ';
-            echo $sql;
-
             $result = mysqli_query($db, $sql);
             if($result)
             {
                 $path = 'images/uploads/body/' . $id . '.jpg';
-                file_put_contents($path, $data);
+                file_put_contents('/var/www/blog/' . $path, $data);
                 $absolute_path = 'https://infinidea.veagle.fr/' . $path;
                 $sql = "UPDATE images SET path = '" . $absolute_path . "' WHERE uid = " . $id;
                 $result = mysqli_query($db, $sql);
