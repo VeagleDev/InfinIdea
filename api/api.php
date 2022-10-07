a93975d3704a9094c79e560f9424d3f34b680373
Pierre <pierre08bt@gmx.co.uk>
2022-10-08 00:53:52 +0200

    add functions to api

--- api/api.php
+++ api/api.php
@@ -23,11 +23,11 @@ if(isset($_POST['comment']) && isset($_POST['article']) && isset($_SESSION['id']
     }
 }
 
-if(isset($_POST['article']) && isset($_GET['action']))
+if((isset($_POST['article']) || isset($_GET['article'])) && isset($_GET['action']))
 {
     if($action = 'likes')
     {
-        // get number of likes
+        $article = SQLpurify(isset($_POST['article']) ? $_POST['article'] : $_GET['article']);
         $sql = "SELECT likes FROM articles WHERE id = $article";
         $result = mysqli_query($db, $sql);
         $likes = mysqli_fetch_assoc($result)['likes'];
