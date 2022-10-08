9ca1e44f344b0d1ca578a155d097fbef12687dab
Pierre <pierre08bt@gmx.co.uk>
2022-10-08 22:16:52 +0200

    fix resize-image.php

--- tools/resize-image.php
+++ tools/resize-image.php
@@ -5,16 +5,13 @@ function load_image($filename, $type)
     $new = 'new34.jpeg';
     if ($type == IMAGETYPE_JPEG) {
         $image = imagecreatefromjpeg($filename);
-        echo "here is jpeg output:";
         imagejpeg($image, $new);
 
     } elseif ($type == IMAGETYPE_PNG) {
         $image = imagecreatefrompng($filename);
-        echo "here is png output:";
         imagepng($image, $new);
     } elseif ($type == IMAGETYPE_GIF) {
         $image = imagecreatefromgif($filename);
-        echo "here is gif output:";
         imagejpeg($image, $new);
     }
     return $new;
@@ -28,9 +25,9 @@ function process_image(string $path, string $id)
     $sd_path = "/var/www/blog/images/uploads/sd/" . $id . ".jpg";
     $thumb_path = "/var/www/blog/images/uploads/thumb/" . $id . ".jpg";
 
-    $hd_size = array(($width > $height ? array(1440, $height * 1440 / $width) : array($width * 1440 / $height, 1440)));
-    $sd_size = array(($width > $height ? array(720, $height * 720 / $width) : array($width * 720 / $height, 720)));
-    $thumb_size = array(($width > $height ? array(360, $height * 360 / $width) : array($width * 360 / $height, 360)));
+    $hd_size = ($width > $height ? array(1440, $height * 1440 / $width) : array($width * 1440 / $height, 1440));
+    $sd_size = ($width > $height ? array(720, $height * 720 / $width) : array($width * 720 / $height, 720));
+    $thumb_size = ($width > $height ? array(360, $height * 360 / $width) : array($width * 360 / $height, 360));
 
     echo "hd_size: " . $hd_size[0] . "x" . $hd_size[1];
     echo "sd_size: " . $sd_size[0] . "x" . $sd_size[1];
