60d166f335c27058fecfeba29e14f80a1f64e3fe
Pierre <pierre08bt@gmx.co.uk>
2022-10-08 17:11:08 +0200

    upload changes

--- explore.php
+++ explore.php
@@ -87,7 +87,7 @@ $db = getDB();
                         <a href="article.php?id=<?php echo($row['id']); ?>" class="gallery-cell">
 
                             <div class="img-container">
-                                <div><img src="images/uploads/<?php echo($row['id']); ?>.jpg" alt="" class="img-article"></div>
+                                <div><img class="img-article" src="images/uploads/<?php echo($row['id']); ?>.jpg" alt="" ></div>
                             </div>
 
                             <div class="text">
