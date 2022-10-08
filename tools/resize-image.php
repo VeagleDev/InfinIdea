acedc2f79f7b3fe6ec0ed83fdf286057216c95cc
Pierre <pierre08bt@gmx.co.uk>
2022-10-08 22:34:00 +0200

    fix resize-image.php

--- tools/resize-image.php
+++ tools/resize-image.php
@@ -1,26 +1,8 @@
 <?php
-
-function load_image($filename, $type)
-{
-    $new = 'new34.jpeg';
-    if ($type == IMAGETYPE_JPEG) {
-        $image = imagecreatefromjpeg($filename);
-        imagejpeg($image, $new);
-
-    } elseif ($type == IMAGETYPE_PNG) {
-        $image = imagecreatefrompng($filename);
-        imagepng($image, $new);
-    } elseif ($type == IMAGETYPE_GIF) {
-        $image = imagecreatefromgif($filename);
-        imagejpeg($image, $new);
-    }
-    return $new;
-}
-
 function process_image(string $path, string $id)
 {
     list($width, $height, $type) = getimagesize($path);
-    $image = load_image($path, $type);
+
     $hd_path = "/var/www/blog/images/uploads/hd/" . $id . ".jpg";
     $sd_path = "/var/www/blog/images/uploads/sd/" . $id . ".jpg";
     $thumb_path = "/var/www/blog/images/uploads/thumb/" . $id . ".jpg";
@@ -29,20 +11,22 @@ function process_image(string $path, string $id)
     $sd_size = ($width > $height ? array(720, round($height * 720 / $width)) : array(round($width * 720 / $height), 720));
     $thumb_size = ($width > $height ? array(360, round($height * 360 / $width)) : array(round($width * 360 / $height), 360));
 
-    echo "hd_size: " . $hd_size[0] . "x" . $hd_size[1] . "<br />";
-    echo "sd_size: " . $sd_size[0] . "x" . $sd_size[1] . "<br />";
-    echo "thumb_size: " . $thumb_size[0] . "x" . $thumb_size[1] . "<br />";
-    echo "default_size: " . $width . "x" . $height;
+    $hd_image = imagecreatetruecolor($hd_size[0], $hd_size[1]);
+    $sd_image = imagecreatetruecolor($sd_size[0], $sd_size[1]);
+    $thumb_image = imagecreatetruecolor($thumb_size[0], $thumb_size[1]);
 
+    $source = imagecreatefromjpeg($path);
+
+    imagecopyresized($hd_image, $source, 0, 0, 0, 0, $hd_size[0], $hd_size[1], $width, $height);
+    imagecopyresized($sd_image, $source, 0, 0, 0, 0, $sd_size[0], $sd_size[1], $width, $height);
+    imagecopyresized($thumb_image, $source, 0, 0, 0, 0, $thumb_size[0], $thumb_size[1], $width, $height);
+
+    imagejpeg($hd_image, $hd_path);
+    imagejpeg($sd_image, $sd_path);
+    imagejpeg($thumb_image, $thumb_path);
+
+    unlink($path);
+
+    return array($hd_path, $sd_path, $thumb_path);
 }
-
-process_image("/var/www/blog/images/Capture.PNG", "1");
-
-/*$filename = "panda.jpeg";
-list($width, $height, $type) = getimagesize($filename);
-echo "Width:", $width, "\n";
-echo "Height:", $height, "\n";
-echo "Type:", $type, "\n";
-
-$old_image = load_image($filename, $type);*/
 
