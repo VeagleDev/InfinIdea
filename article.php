0182e4146c01cf44427ace5dd0b91d3169eb59a0
Pierre <pierre08bt@gmx.co.uk>
2022-10-24 19:22:36 +0200

    remove debug

--- article.php
+++ article.php
@@ -81,8 +81,6 @@ $db = getDB();
             {
                 $sql = "SELECT * FROM articles WHERE uid = '$aid'"; // On récupère les infos de l'article
                 $result = mysqli_query($db, $sql); // On exécute la requête
-                echo '<p> Request : ' . $sql . '</p>';
-                echo '<p> Nombre de résultats : ' . mysqli_num_rows($result) . '</p>';
                 if ($result) // Si Il existe
                 {
                     $row = mysqli_fetch_assoc($result); // On récupère les infos
