5569af741fdd9e30c50eb048ae810151cbe5d042
Pierre <pierre08bt@gmx.co.uk>
2022-10-27 01:06:06 +0200

    avancée new-write.php

--- backend/js/write.js
+++ backend/js/write.js
@@ -1,71 +1,71 @@
 // eventlistener to click event on the submit button
 console.log('salut');
 
-document.onload = function () {
-    document.getElementsByClassName("submit")[0].addEventListener('click', () => {
-        console.log('bouton cliqué');
-        const title = document.getElementsByClassName("title")[0].value
-        const description = document.getElementsByClassName("description")[0].value
-        const content = document.getElementsByClassName("content")[0].value
-        const tags = document.getElementsByClassName("tags")[0].value
 
-        const title_regex = /^.{4,50}$/
-        const description_regex = /^.{4,200}$/
-        const content_regex = /^.{30,10000}$/
-        const tags_regex = /^.{0,1000}$/
+document.getElementsByClassName("submit")[0].addEventListener('click', () => {
+    console.log('bouton cliqué');
+    const title = document.getElementsByClassName("title")[0].value
+    const description = document.getElementsByClassName("description")[0].value
+    const content = document.getElementsByClassName("content")[0].value
+    const tags = document.getElementsByClassName("tags")[0].value
 
-        let legit = true;
+    const title_regex = /^.{4,50}$/
+    const description_regex = /^.{4,200}$/
+    const content_regex = /^.{30,10000}$/
+    const tags_regex = /^.{0,1000}$/
 
-        if (!title_regex.test(title)) {
-            // display error message
-            document.getElementsByClassName("title-error")[0].style.display = "block"
-            legit = false;
-        } else {
-            document.getElementsByClassName("title-error")[0].style.display = "none"
-        }
-        if (!description_regex.test(description)) {
-            document.getElementsByClassName("description-error")[0].style.display = "block"
-            legit = false;
-        } else {
-            document.getElementsByClassName("description-error")[0].style.display = "none"
-        }
-        if (!content_regex.test(content)) {
-            document.getElementsByClassName("content-error")[0].style.display = "block"
-            legit = false;
-        } else {
-            document.getElementsByClassName("content-error")[0].style.display = "none"
-        }
-        if (!tags_regex.test(tags)) {
-            document.getElementsByClassName("tags-error")[0].style.display = "block"
-            legit = false;
-        } else {
-            document.getElementsByClassName("tags-error")[0].style.display = "none"
-        }
+    let legit = true;
 
-        if (legit) {
-            jQuery.ajax({
-                url: '/backend/php/publish-article.php',
-                type: 'POST',
-                data: {
-                    title: title,
-                    description: description,
-                    content: content,
-                    tags: tags
-                },
-                success: function (data) {
-                    alert('Yes')
-                },
-                error: function (data) {
-                    alert('No')
-                }
-            });
-            return true;
+    if (!title_regex.test(title)) {
+        // display error message
+        document.getElementsByClassName("title-error")[0].style.display = "block"
+        legit = false;
+    } else {
+        document.getElementsByClassName("title-error")[0].style.display = "none"
+    }
+    if (!description_regex.test(description)) {
+        document.getElementsByClassName("description-error")[0].style.display = "block"
+        legit = false;
+    } else {
+        document.getElementsByClassName("description-error")[0].style.display = "none"
+    }
+    if (!content_regex.test(content)) {
+        document.getElementsByClassName("content-error")[0].style.display = "block"
+        legit = false;
+    } else {
+        document.getElementsByClassName("content-error")[0].style.display = "none"
+    }
+    if (!tags_regex.test(tags)) {
+        document.getElementsByClassName("tags-error")[0].style.display = "block"
+        legit = false;
+    } else {
+        document.getElementsByClassName("tags-error")[0].style.display = "none"
+    }
 
-        } else {
-            alert('Not legit')
-            return false;
-        }
+    if (legit) {
+        jQuery.ajax({
+            url: '/backend/php/publish-article.php',
+            type: 'POST',
+            data: {
+                title: title,
+                description: description,
+                content: content,
+                tags: tags
+            },
+            success: function (data) {
+                alert('Yes')
+            },
+            error: function (data) {
+                alert('No')
+            }
+        });
+        return true;
+
+    } else {
+        alert('Not legit')
+        return false;
+    }
 
 
-    })
-}
+})
+
--- tools/new-write.php
+++ tools/new-write.php
@@ -19,10 +19,10 @@ logs('Commence l\'\'écriture d\'\'un article');
     <title>Infinidea : Créer</title>
     <link href="/css/header.css" rel="stylesheet">
     <link href="/css/write.css" rel="stylesheet">
-    <script src="/backend/library/jquery-3.6.1.min.js" defer></script>
-    <script src="/backend/js/paste-image.js" defer></script>
-    <script src="/backend/js/write.js" defer></script>
-    <script src="/backend/js/fontawesome.js" async></script>
+    <script src="/backend/library/jquery-3.6.1.min.js"></script>
+    <script src="/backend/js/paste-image.js"></script>
+    <script src="/backend/js/write.js"></script>
+    <script src="/backend/js/fontawesome.js"></script>
 </head>
 <body>
 <section class="top-page">
