663a3b1467229bfa204d36ec396b2fc0faf73615
Pierre <contact@veagle.fr>
2022-10-29 01:53:21 +0200

    Ajustements noms et CSS

--- backend/js/start-article.js
+++ backend/js/start-article.js
@@ -1,15 +1,3 @@
-/*document.onload = () => {
-
-    // select all pre elements and add the prettyprint class without ajax
-    document.querySelectorAll("pre").forEach((el) => {
-        el.classList.add("prettyprint")
-    })
-    document.querySelectorAll("code").forEach((el) => {
-        el.classList.add("prettyprint")
-    })
-
-    PR.prettyPrint();
-}*/
 
 window.onload = () => {
     document.querySelectorAll("pre").forEach((el) => {
