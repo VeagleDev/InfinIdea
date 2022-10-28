aae4e579c57a3d9c43bc6118116233719119ade2
Pierre <contact@veagle.fr>
2022-10-28 23:42:49 +0200

    Tentative d'erreur PHPMD

---
+++ TEST/mess.php
@@ -0,0 +1,20 @@
+<?php
+
+// create a full of errors file to trigger PHPMD
+
+fun main(){
+println("Hello, World!")
+}
+
+val x = 1
+
+    val y = 2
+
+    val z = 3
+
+    for i in range(1, 10){
+    println(i)
+    }
+
+    file_exists(fziujo),
+
