cceb6bf05c7ac270f6d77b2ab0e7a0303ff23d6d
pierrbt <pierre08bt@gmx.co.uk>
2022-10-29 03:00:18 +0200

    Configuration highlight.js

--- article.php
+++ article.php
@@ -28,8 +28,8 @@ $db = getDB();
     <script type="text/javascript" src=/backend/library/jquery-3.6.1.min.js></script>
     <script type="text/javascript" src="/backend/js/start-article.js"></script>
     -->
-    <link rel="stylesheet" href="/path/to/styles/default.min.css">
-    <script src="/path/to/highlight.min.js"></script>
+    <link rel="stylesheet" href="backend/library/highlight.js/highlight.min.js">
+    <script src="backend/library/highlight.js/styles/atom-one-dark.min.css"></script>
     <script>hljs.highlightAll();</script>
 
 </head>
