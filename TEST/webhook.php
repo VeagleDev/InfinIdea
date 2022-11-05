d3146424f35eaf176d3d8905f332f735a05feddf
pierrbt <pierre08bt@gmx.co.uk>
2022-11-05 15:40:55 +0100

    Ajout d'un test de webhook

--- TEST/webhook.php
+++ TEST/webhook.php
@@ -3,8 +3,7 @@ set_include_path('/var/www/blog');
 require_once 'tools/tools.php';
 
 if (isset($_POST['payload'])) {
-    $json = json_decode(HTMLpurify(['payload']));
-    // get result of the git pull command
+    $json = json_decode(HTMLpurify(['payload']), true);
     $result = shell_exec('git pull');
     // add result to the json
     // create new array with the result string, the result code and the date
@@ -13,6 +12,7 @@ if (isset($_POST['payload'])) {
     // add new element to the json array withour overwriting the old one
     $json['result'] = str_replace("\n", " - ", $result);
     $json['date'] = date('Y-m-d H:i:s');
+
 
     $file = fopen('webhook.json', 'w');
     fwrite($file, json_encode($json));
@@ -23,6 +23,8 @@ if (isset($_POST['payload'])) {
     if (file_exists('webhook.json')) {
         $json = file_get_contents('webhook.json');
         header('Content-Type: application/json');
+        header('Server: Wow such server');
+        header('X-Powered-By: PHP');
         echo $json;
 
     } else {
